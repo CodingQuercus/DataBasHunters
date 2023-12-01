66ba334d0acae58e2a24219589cb370cafc33fc5
eddankurresson <104197104+eddankurresson@users.noreply.github.com>
2023-12-01 11:59:24 +0100

    sdfg

---
+++ DataBasHunters/Shared/User.cs
@@ -0,0 +1,17 @@
+﻿namespace DataBasHunters.Shared;
+
+public class User
+{
+    public int Id { get; set; }
+
+    public int Funds { get; set; }
+
+    public string? Usernamne { get; set; }
+
+    public string? Password { get; set; }
+
+    public string? Profilepicture { get; set; }
+
+
+}
+
