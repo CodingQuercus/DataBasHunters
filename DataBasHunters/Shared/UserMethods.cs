ca1200fc3e706e6578121c8f79ec4e4aaf542a13
mogge1337 <morgan.burlin@gmail.com>
2023-12-06 14:36:03 +0100

    CRYPIC FLIp

--- DataBasHunters/Shared/UserMethods.cs
+++ DataBasHunters/Shared/UserMethods.cs
@@ -31,7 +31,7 @@ namespace DataBasHunters.Shared
                     {
                         Id = Convert.ToInt16(reader["Id"]),
                         Username = reader["Username"].ToString(),
-                        Password = reader["Password"].ToString(),
+                        Password = Cryptography.Decrypt(reader["Password"].ToString()),
                         Funds = Convert.ToInt32(reader["Funds"])
                     };
                     userList.Add(user);
@@ -59,14 +59,14 @@ namespace DataBasHunters.Shared
             SqlCommand dbCommand = new SqlCommand(sqlstring, dbConnection);
 
             dbCommand.Parameters.Add("@Username", SqlDbType.NVarChar, 50).Value = user.Username;
-            dbCommand.Parameters.Add("@Password", SqlDbType.NVarChar, 50).Value = user.Password;
+            dbCommand.Parameters.Add("@Password", SqlDbType.NVarChar, 50).Value = Cryptography.Encrypt(user.Password);
 
             try
             {
                 dbConnection.Open();
                 int i = dbCommand.ExecuteNonQuery();
                 if (i == 1) { errormsg = ""; }
-                else { errormsg = "Failed to create game, try again"; };
+                else { errormsg = "Failed to create user, try again"; };
                 return i;
             }
             catch (Exception e)
@@ -88,7 +88,7 @@ namespace DataBasHunters.Shared
             SqlCommand dbCommand = new SqlCommand(sqlstring, dbConnection);
 
             dbCommand.Parameters.Add("@Username", SqlDbType.NVarChar, 50).Value = user.Username;
-            dbCommand.Parameters.Add("@Password", SqlDbType.NVarChar, 50).Value = user.Password;
+            dbCommand.Parameters.Add("@Password", SqlDbType.NVarChar, 50).Value = Cryptography.Encrypt(user.Password);
 
             try
             {
@@ -109,6 +109,79 @@ namespace DataBasHunters.Shared
                 dbConnection.Close();
             }
         }
+
+        // DONT USE
+        public void EncryptUsers(out string errormsg)
+        {
+            SqlConnection dbConnection = new SqlConnection();
+            dbConnection.ConnectionString = @"Server=tcp:basehunters.database.windows.net,1433;Initial Catalog=databasprojekt;Persist Security Info=False;User ID=hunters;Password=COOLkille15;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+            string selectSql = "SELECT [Id], [Username], [Password], [Funds] FROM [User]";
+            string updateSql = "UPDATE [dbo].[User] SET [Password] = @NewPassword WHERE [Id] = @UserId";
+
+            List<User> userList = new List<User>();
+
+            try
+            {
+                dbConnection.Open();
+                using (SqlCommand selectCommand = new SqlCommand(selectSql, dbConnection))
+                {
+                    SqlDataReader reader = selectCommand.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        User user = new User
+                        {
+                            Id = Convert.ToInt16(reader["Id"]),
+                            Username = reader["Username"].ToString(),
+                            Password = reader["Password"].ToString(),
+                            Funds = Convert.ToInt32(reader["Funds"])
+                        };
+                        userList.Add(user);
+                    }
+                }
+                errormsg = "";
+            }
+            catch (Exception e)
+            {
+                errormsg = e.Message;
+                return;
+            }
+            finally
+            {
+                dbConnection.Close();
+            }
+
+            try
+            {
+                dbConnection.Open();
+                using (SqlCommand updateCommand = new SqlCommand(updateSql, dbConnection))
+                {
+                    foreach (User user in userList)
+                    {
+                        // Kryptera lösenordet för varje användare
+                        string encryptedPassword = Cryptography.Encrypt(user.Password);
+
+                        // Återanvänd samma updateCommand, rensa parametrar innan varje ny användning
+                        updateCommand.Parameters.Clear();
+                        updateCommand.Parameters.AddWithValue("@NewPassword", encryptedPassword);
+                        updateCommand.Parameters.AddWithValue("@UserId", user.Id);
+
+                        updateCommand.ExecuteNonQuery();
+                    }
+                }
+                errormsg = "";
+            }
+            catch (Exception e)
+            {
+                errormsg = e.Message;
+            }
+            finally
+            {
+                dbConnection.Close();
+            }
+        }
+
     }
 
 }
