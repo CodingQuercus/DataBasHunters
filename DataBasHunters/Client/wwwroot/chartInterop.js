b1b3a624da6e35b69b25f1278ebe47a3fe940dda
eddankurresson <104197104+eddankurresson@users.noreply.github.com>
2023-12-13 12:34:22 +0100

    fixa bomba

--- DataBasHunters/Client/wwwroot/chartInterop.js
+++ DataBasHunters/Client/wwwroot/chartInterop.js
@@ -1,17 +1,8 @@
-function createChart(chartElement, chartType, data, options) {
-    console.log("Chart Element:", chartElement);
-    console.log("Chart Type:", chartType);
-    console.log("Data:", data);
-    console.log("Options:", options);
-
-    if (chartElement) {
-        var ctx = chartElement.getContext('2d');
-        return new Chart(ctx, {
-            type: chartType,
-            data: data,
-            options: options
-        });
-    } else {
-        console.error('Canvas element not found');
-    }
+function createChart(canvasId, chartType, data, options) {
+    var ctx = document.getElementById(canvasId).getContext('2d');
+    return new Chart(ctx, {
+        type: chartType,
+        data: data,
+        options: options
+    });
 }
